2274773aadb1fb04df9c5d22fcf3dd947a470bac
travis <travissomaroo2022@gmail.com>
2024-01-23 16:05:56 +0200

    Initial setup

---
+++ src/main/java/com/travis/SpringPlayground/SpringPlaygroundApplication.java
@@ -0,0 +1,16 @@
+package com.travis.SpringPlayground;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class SpringPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPlaygroundApplication.class, args);
+		log.info("***** READY TO QUERY *****");
+	}
+
+}
