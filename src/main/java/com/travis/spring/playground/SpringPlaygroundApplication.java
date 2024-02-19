34b7ed46dbff55ef8a7d980b22e27a5787e42cad
travis <travissomaroo2022@gmail.com>
2024-02-19 19:16:49 +0200

    Moved faker config to its own class

--- src/main/java/com/travis/spring/playground/SpringPlaygroundApplication.java
+++ src/main/java/com/travis/spring/playground/SpringPlaygroundApplication.java
@@ -1,23 +1,17 @@
 package com.travis.spring.playground;
 
-import com.github.javafaker.Faker;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @Slf4j
 @SpringBootApplication
 public class SpringPlaygroundApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringPlaygroundApplication.class, args);
-		log.info("***** READY TO QUERY *****");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPlaygroundApplication.class, args);
+        log.info("***** READY TO QUERY *****");
+    }
 
 
-	@Bean
-	public Faker faker() {
-		return new Faker();
-	}
 }
