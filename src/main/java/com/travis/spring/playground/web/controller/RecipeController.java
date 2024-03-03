02efa77a5028e2a6632dcc8aab70ca56b70207ab
travis <travissomaroo2022@gmail.com>
2024-03-03 22:25:59 +0200

    Added exception handler

--- src/main/java/com/travis/spring/playground/web/controller/RecipeController.java
+++ src/main/java/com/travis/spring/playground/web/controller/RecipeController.java
@@ -1,13 +1,18 @@
 package com.travis.spring.playground.web.controller;
 
+import com.travis.spring.playground.exception.NotFoundException;
 import com.travis.spring.playground.model.recipe.Recipe;
 import com.travis.spring.playground.service.RecipeService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 import java.util.Set;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/recipes")
@@ -29,5 +34,12 @@ public class RecipeController {
     public String deleteRecipe(@PathVariable Long recipeId) {
         recipeService.deleteById(recipeId);
         return "Recipe deleted";
+    }
+
+    // This is for testing purposes
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFound(NotFoundException ex) {
+        String errorMessage = "Resource not found: " + ex.getMessage();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
     }
 }
